0230ba390f4bcbd77a888e71476b83337ed663e9
Joe <6955350+joebutler2@users.noreply.github.com>
2020-04-24 11:51:48 -0400

    Refactor Clojure - word count exercise.

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -2,15 +2,11 @@
   (:require [clojure.string :as str]))
 
 (defn- my-frequencies [words]
-  (reduce
-    #(update %1 %2 (fn [v] (inc v)))
-    (zipmap words (repeat 0))
-    words))
+  (reduce #(update % %2 (fnil inc 0)) {} words))
 
 (defn word-count [s]
-  (-> s
-      (str/replace #"[,!&@\$%\^:]" "")
-      (str/split #"\s+")
-      (#(map str/lower-case %))
+  (->> s
+      (str/lower-case)
+      (re-seq #"\w+")
       (my-frequencies)))
 
