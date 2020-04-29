420084286b1e81ccfdeeb95c0a3c73030809c876
Joe <6955350+joebutler2@users.noreply.github.com>
2020-04-29 13:04:23 -0400

    Refactor clojure - anagram.

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,9 +1,18 @@
 (ns anagram
   (:require [clojure.string :as str]))
 
-(defn anagrams-for [word prospect-list]
-  (let [lowered-word (str/lower-case word)
-        sorted-word (sort lowered-word)]
+(defn- anagram? [word]
+  (let [sorted-word (sort word)]
+    #(= sorted-word (sort (str/lower-case %)))))
+
+(defn- prune-prospects [word prospects]
+  (remove
+    #(= (str/lower-case %) word)
+    prospects))
+
+(defn anagrams-for [word prospects]
+  (let [lowered-word (str/lower-case word)]
     (filter
-      #(= sorted-word (sort (str/lower-case %)))
-      (->> prospect-list (remove #(= (str/lower-case %) lowered-word))))))
+      (anagram? lowered-word)
+      (prune-prospects lowered-word prospects))))
+
